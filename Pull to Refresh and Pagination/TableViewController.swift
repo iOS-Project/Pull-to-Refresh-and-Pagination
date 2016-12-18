e4d671c7139ac0e746dffd61c418f186f9846485
Lun Sovathana <sovathanalun@gmail.com>
2016-12-18 12:05:33 +0700

    Add UIActivityIndicatorView

--- Pull to Refresh and Pagination/TableViewController.swift
+++ Pull to Refresh and Pagination/TableViewController.swift
@@ -11,12 +11,13 @@ import UIKit
 class TableViewController: UITableViewController {
 
     var page = 1
-    var limit = 50
+    var limit = 15
     var totalPage = 10
 
     var items = [Int]()
     
     var paggingView : UIActivityIndicatorView?
+    var searchController : UISearchController?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,58 +33,32 @@ class TableViewController: UITableViewController {
         tableView.refreshControl?.addTarget(self, action: #selector(refresh), for: UIControlEvents.valueChanged)
         
         // Add Pagination
-        paggingView = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+        addPagination()
+        
+        addSearcchController()
+    }
+    
+    func addPagination(){
+        let activityView = UIView()
+        var frame = activityView.frame
+        frame.size.width = tableView.bounds.width
+        frame.size.height = 50
+        activityView.frame = frame
+        activityView.backgroundColor = UIColor.red
+
+        paggingView = UIActivityIndicatorView(activityIndicatorStyle: .whiteLarge)
+        paggingView?.center = activityView.center
         paggingView?.tintColor = UIColor.black
-        paggingView?.hidesWhenStopped = true
-        tableView.addSubview(paggingView!)
+        //paggingView?.hidesWhenStopped = true
+        paggingView?.startAnimating()
+        activityView.addSubview(paggingView!)
+        tableView.tableFooterView = activityView
     }
     
-    func refresh(){
-        // Show refresh animation
-        tableView.refreshControl?.beginRefreshing()
-        // Load Data from Service
-        
-        let timer = Timer.init(timeInterval: 10, target: self, selector: #selector(updateData), userInfo: nil, repeats: false)
-        
-        timer.fire()
-    }
-    
-    func updateData(){
-        // When Delegate from Service stop the refresh
-        tableView.refreshControl?.endRefreshing()
-    }
-    
-    // Pagination
-    func pagging(){
-        if page < totalPage{
-            page += 1
-            paggingView?.startAnimating()
-            loadData(page: page, limit: limit)
-        }
-    }
-    
-    // Suppost this func is server that retrieve from API
-    func loadData(page: Int, limit: Int){
-        
-        var arr = [Int]()
-        
-        for i in 1...limit{
-            arr.append(i)
-        }
-        
-        // Call Delegate
-        paggingDidEnd(data: arr)
-        
-    }
-    
-    func paggingDidEnd(data : [Int]){
-        
-        for i in data {
-            items.append(i)
-        }
-        
-        paggingView?.stopAnimating()
-        self.tableView.reloadData()
+    func addSearcchController(){
+        searchController = UISearchController(searchResultsController: nil)
+        searchController?.searchBar.delegate = self
+        tableView.tableHeaderView = searchController?.searchBar
     }
     
     override func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
@@ -108,8 +83,67 @@ class TableViewController: UITableViewController {
         return cell
     }
     
+}
+
+extension TableViewController{
+    func refresh(){
+        // Show refresh animation
+        tableView.refreshControl?.beginRefreshing()
+        // Load Data from Service
+        
+        let timer = Timer.init(timeInterval: 1000, target: self, selector: #selector(updateData), userInfo: nil, repeats: false)
+        
+        timer.fire()
+    }
     
+    func updateData(){
+        // When Delegate from Service stop the refresh
+        tableView.refreshControl?.endRefreshing()
+    }
     
+    // Pagination
+    func pagging(){
+        if page < totalPage{
+            page += 1
+            
+            tableView.tableFooterView?.isHidden = false
+            
+            let timer = Timer.init(timeInterval: 10, repeats: false, block: { timer in
+                self.loadData(page: self.page, limit: self.limit)
+            })
+            
+            timer.fire()
+        }
+    }
     
+    // Suppost this func is server that retrieve from API
+    func loadData(page: Int, limit: Int){
+        
+        var arr = [Int]()
+        
+        for i in 1...limit{
+            arr.append(i)
+        }
+        
+        // Call Delegate
+        paggingDidEnd(data: arr)
+        
+    }
     
+    func paggingDidEnd(data : [Int]){
+        
+        for i in data {
+            items.append(i)
+        }
+        
+        tableView.tableFooterView?.isHidden = false
+        
+        self.tableView.reloadData()
+    }
+}
+
+extension TableViewController : UISearchBarDelegate{
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        print("Cancel Clicked")
+    }
 }
